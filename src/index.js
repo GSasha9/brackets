2b858a438c991ddf49ac42a4523e8984c6be7de2
Sasha Hurbanava <sasha.hurbnva@gmail.com>
2024-09-16 12:03:54 +0300

    feat: 1 try

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  //если в строке нечетное кол-во символов - возвращаем false
+  if(str.length%2 != 0){
+    return false;
+  }
+  
+  
+  let openBrackets = [];
+  let closeBrackets = [];
+
+  let arr = [];
+
+  bracketsConfig.forEach(([open, close]) => {
+  openBrackets.push(open);
+  closeBrackets.push(close);
+  });
+
+  for (let bracket of str) {
+    if (openBrackets.includes(bracket)) {
+      arr.push(bracket);
+    } 
+    else {
+      let lastOpenBracket = arr.pop();
+      let compareCloseBracket = closeBrackets[openBrackets.indexOf(lastOpenBracket)];
+      if (bracket !== compareCloseBracket) {
+        return false;
+      }
+    }
+  }
+
+return arr.length === 0;
+
+
 }
+
